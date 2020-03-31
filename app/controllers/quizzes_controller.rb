5bba6954ee0e6015556392f8d69419c62269a9e5
OlliePorter <ol.porter@me.com>
2020-03-31 16:14:31 +0100

    Add create route for quiz with bootstrap flash errors & 1st feature test

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -5,4 +5,17 @@ class QuizzesController < ApplicationController
   def new
     @quiz = Quiz.new
   end
+
+  def create
+    @quiz = Quiz.new(quiz_params)
+    @quiz.save
+    flash[:success] = "Quiz has been created"
+    redirect_to quizzes_path
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:name, :description)
+  end
 end
---
+++ spec/features/creating_quiz_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Creating questions" do
+  scenario "A new quiz is created" do
+    visit "/"
+
+    click_link "New Quiz"
+
+    fill_in "Name", with: "Quiz 1"
+    fill_in "Description", with: "This is a description for quiz 1"
+    click_button "Create Quiz"
+
+    expect(page).to have_content("Quiz has been created")
+    expect(page.current_path).to eq(quizzes_path)
+  end
+end
